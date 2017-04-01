0dff63775fc0518ebb72e41a24cc94927066926e
Aaron Olsen <mraaronolsen@gmail.com>
2017-04-01 07:46:10 -0600

    starting board tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,4 +1,40 @@
 require_relative 'helper'
 
 class Board
+
+  attr_accessor :cells
+
+  def initialize(size = 5)
+    @size = 5
+    @cells = {}
+  end
+
+  def build
+    @size.times do |row|
+      @cells[ (row+97).chr ] = (row+97).chr
+      @size.times do |col|
+        @cells[ (row+97).chr + (col+1).to_s ] = " "
+      end
+    end
+  end
+
+  def top
+    (0..@size).collect do |i|
+      if i.zero? then ' .' else i.to_s.rjust(2) end
+    end
+  end
+
+  def edge(cell)
+    (cell[1..-1].to_i).modulo(@size+1).zero?
+  end
+
+  def display
+    print top.join(" ")
+
+    @cells.each do |cell, value|
+      print "\n" if edge(cell)
+      print "#{value}".center(3)
+    end
+  end
+
 end
--- test/board_test.rb
+++ test/board_test.rb
@@ -1,12 +1,55 @@
 require_relative 'test_helper'
+require_relative 'board_test_layouts'
 
 class BoardTest < Minitest::Test
 
+  def setup
+
+  end
+
   def test_that_it_is_a_board
     board = Board.new
-
     assert_instance_of Board, board
   end
 
-  def test_that
+  def test_that_it_has_a_size
+    board = Board.new
+    assert_equal board.size, 5
+  end
+
+  def test_that_it_can_have_a_different_size
+    board = Board.new(6)
+    assert_equal board.size, 6
+  end
+
+  def test_that_it_can_not_be_bigger_than_12
+    board = Board.new(26)
+    assert_equal board.size, 12
+  end
+
+  def test_that_it_can_not_be_smaller_than_5
+    board = Board.new(4)
+    assert_equal board.size, 5
+  end
+
+  def test_that_board_hass_no_cells_when_created
+    board = Board.new
+    assert board.cells.empty?
+  end
+
+  def test_that_it_builds_default_min_board_size
+    board = Board.new
+    assert_equal board.cells, BoardTestLayouts::min
+  end
+
+  def test_that_it_builds_max_board_size
+    board = Board.new(12)
+    assert_equal board.cells, BoardTestLayouts::max
+  end
+
+
+
+end
+
+
 end
